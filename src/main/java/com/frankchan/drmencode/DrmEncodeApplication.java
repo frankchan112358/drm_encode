a6b38882964f6b0be430018e3be8504e7c147269
Frank Chan <frank.chan@hy-tech.com.tw>
2022-10-12 15:07:53 +0800

    init spring boot proj

---
+++ src/main/java/com/frankchan/drmencode/DrmEncodeApplication.java
@@ -0,0 +1,13 @@
+package com.frankchan.drmencode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrmEncodeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DrmEncodeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/frankchan/drmencode/DrmEncodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.frankchan.drmencode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrmEncodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
